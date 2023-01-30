d61a7f5366ede33bc0e33492194a2ce3e1dfbf90
alanlimauno <alan.lima@unosquare.com>
2023-01-30 17:18:27 -0600

    First deliverable with given, when, then

---
+++ src/main/java/tests/RestTests.java
@@ -0,0 +1,51 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RestTests {
+
+    @BeforeClass
+    public void setBase() {
+        RestAssured.baseURI = "https://reqres.in/api/";
+    }
+
+    @Test
+    public void validateUser() {
+        given()
+                .when()
+                .get("users/2")
+                .then()
+                .assertThat().statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data.id", equalTo(2))
+                .body("data.email", equalTo("janet.weaver@reqres.in"))
+                .body("data.first_name", equalTo("Janet"))
+                .body("data.last_name", equalTo("Weaver"))
+                .body("data.avatar", equalTo("https://reqres.in/img/faces/2-image.jpg"))
+                .body("support.url", equalTo("https://reqres.in/#support-heading"))
+                .body("support.text", equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
+    }
+
+    @Test
+    public void validateUnknown() {
+        given()
+                .when()
+                .get("unknown/2")
+                .then()
+                .assertThat().statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data.id", equalTo(2))
+                .body("data.name", equalTo("fuchsia rose"))
+                .body("data.year", equalTo(2001))
+                .body("data.color", equalTo("#C74375"))
+                .body("data.pantone_value", equalTo("17-2031"))
+                .body("support.url", equalTo("https://reqres.in/#support-heading"))
+                .body("support.text", equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
+    }
+}
